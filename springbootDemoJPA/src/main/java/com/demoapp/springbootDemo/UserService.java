f8c4ba01cd2cab9eb8537fb9470a12a24c713c06
pdiemi <phamdiem0902@gmail.com>
2021-01-07 12:48:41 -0600

    added @Transactional to UserService in springbootDemoJPA

--- springbootDemoJPA/src/main/java/com/demoapp/springbootDemo/UserService.java
+++ springbootDemoJPA/src/main/java/com/demoapp/springbootDemo/UserService.java
@@ -5,8 +5,10 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 public class UserService {
 
 	// private HashMap<String, String> userList = new HashMap<String, String>();
