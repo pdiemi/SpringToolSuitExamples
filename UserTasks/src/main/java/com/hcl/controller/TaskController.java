3fb044c6c1ac6afd98ad9883f0f89a4bd65131b0
pdiemi <phamdiem0902@gmail.com>
2021-01-15 23:29:16 -0600

    added task controller

--- UserTasks/src/main/java/com/hcl/controller/TaskController.java
+++ UserTasks/src/main/java/com/hcl/controller/TaskController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.hcl.dao.TaskDao;
@@ -17,7 +18,7 @@ public class TaskController {
 
 	@Autowired
 	private TaskDao taskDao;
-	
+
 	@GetMapping("/{user}/all-tasks")
 	public ModelAndView getAllTasks(@PathVariable String user) {
 		List<Task> allTasks = taskDao.findAll();
@@ -27,6 +28,19 @@ public class TaskController {
 				taskList.add(task);
 			}
 		}
-		return new ModelAndView("dashboard","taskList", taskList);
+		return new ModelAndView("dashboard", "taskList", taskList);
+	}
+
+	@GetMapping("/{user}/add-task")
+	public ModelAndView addTask(@PathVariable String user) {
+		Task newTask = new Task();
+		newTask.setTaskEmail(user);
+		return new ModelAndView("taskForm", "newTask", newTask);
+	}
+	
+	@PostMapping("/{user}/add-task")
+	public String addTask(Task newTask) {
+		taskDao.save(newTask);
+		return "redirect:/"+newTask.getTaskEmail()+"/all-tasks";
 	}
 }
--- UserTasks/src/main/java/com/hcl/model/Task.java
+++ UserTasks/src/main/java/com/hcl/model/Task.java
@@ -10,10 +10,12 @@ import javax.persistence.Id;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @ToString
 @Entity
 public class Task {
